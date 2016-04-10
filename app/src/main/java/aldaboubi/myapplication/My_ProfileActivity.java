f9d95a59f271f864b3cf61c0ef32dd5623ce9767
aldaboubi <xkeedox@hotmail.com>
2016-04-10 23:45:41 +0300

    design my  profile 2 image button : 1- request 2- donate
    request button move to RecieveActivity

--- app/src/main/java/aldaboubi/myapplication/My_ProfileActivity.java
+++ app/src/main/java/aldaboubi/myapplication/My_ProfileActivity.java
@@ -7,27 +7,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 
 public class My_ProfileActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
+    DrawerLayout profileLayout;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    ImageButton request;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_profile);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Drawer Navigation
+        // set new ToolBar
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        setSupportActionBar(toolbar);
+        // create Drawer Navigation in My Profile Layout
+            profileLayout = (DrawerLayout) findViewById(R.id.myProfile_layout);
+            actionBarDrawerToggle = new ActionBarDrawerToggle(this, profileLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+            profileLayout.setDrawerListener(actionBarDrawerToggle);
+        // END create Drawer Navigation in My Profile Layout
+        // Drawer Navigation
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
+        // image Button go to RecieveActivity
+        request = (ImageButton) findViewById(R.id.request);
+        request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent req = new Intent(My_ProfileActivity.this, RecieveActivity.class);
+                startActivity(req);
+            }
+        });
+        // image Button go to RecieveActivity
     }
 
     @Override
@@ -37,9 +55,7 @@ public class My_ProfileActivity extends AppCompatActivity {
 
     }
 
-    public void recieve(View view) {
-        Intent i = new Intent(this, RecieveActivity.class);
-        startActivity(i);
-    }
+
+
 
 }
